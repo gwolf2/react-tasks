be9aebd71b77ec68b896ece72aa1f54d2cc2bc7d
Gavin Wolf <gavinwolf@Gavins-MacBook-Pro.local>
2017-08-11 16:25:30 -0400

    Create basic interface

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import {Appbar, Container} from "muicss/react";
+import Tasks from './Components/Tasks';
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Appbar>
+          <Container>
+            <table width="100%">
+              <tbody>
+              <tr>
+                <td className="mui--appbar-height"><h3>ReactTasks</h3></td>
+              </tr>
+              </tbody>
+            </table>
+          </Container>
+        </Appbar>
+        <br />
+        <Container>
+          <Tasks />
+        </Container>
       </div>
     );
   }
---
+++ src/Components/Tasks.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import {Panel} from "muicss/react";
+
+class Tasks extends Component {
+  render() {
+    return (
+      <Panel>
+        TASKS
+      </Panel>
+    );
+  }
+}
+
+export default Tasks;
