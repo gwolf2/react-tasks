8e6b0f14d2b5d658a4ff191f8bb0e078187acb55
Gavin Wolf <gavinwolf@Gavins-MacBook-Pro.local>
2017-08-11 20:08:45 -0400

    Completed app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from "react";
 import axios from "axios";
-import {Appbar, Container} from "muicss/react";
+import {Appbar, Container, Button} from "muicss/react";
 import Tasks from "./Components/Tasks";
+import AddTask from "./Components/AddTask";
 import "./App.css";
 
 class App extends Component {
@@ -50,6 +51,49 @@ class App extends Component {
     });
   }
 
+  addTask(text){
+    axios.request({
+      method: 'post',
+      url: 'https://api.mlab.com/api/1/databases/reacttasks/collections/tasks?apiKey=mb7H00lW6jqlpY2bgWAmNKfIE7YvH29m',
+      data: {
+        text: text,
+        completed: false
+      }
+    }).then((response) => {
+      let tasks = this.state.tasks;
+      tasks.push({
+        _id: response.data._id,
+        text: text,
+        completed: false
+      });
+      this.setState({tasks: tasks});
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  clearTasks(){
+    let tasks = this.state.tasks;
+    let i = tasks.length;
+
+    while(i--) {
+      if(tasks[i].completed === true) {
+        let id = tasks[i]._id.$oid;
+        tasks.splice(i, 1);
+        axios.request({
+          method: 'delete',
+          url: 'https://api.mlab.com/api/1/databases/reacttasks/collections/tasks/' + id + '?apiKey=mb7H00lW6jqlpY2bgWAmNKfIE7YvH29m'
+        }).then((response) => {
+
+        }).catch((error) => {
+          console.log(error);
+        });
+      }
+    }
+
+    this.setState({tasks: tasks});
+  }
+
   render() {
     return (
       <div className="App">
@@ -66,7 +110,9 @@ class App extends Component {
         </Appbar>
         <br />
         <Container>
-          <Tasks onEditState={this.editState.bind(this)} tasks={this.state.tasks}/>
+          <AddTask onAddTask={this.addTask.bind(this)} />
+          <Tasks onEditState={this.editState.bind(this)} tasks={this.state.tasks} />
+          <Button color="danger" onClick={this.clearTasks.bind(this)}>Clear Completed Tasks</Button>
         </Container>
       </div>
     );
